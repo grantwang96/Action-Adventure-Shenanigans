b75e6d2b381feb1714c62fe65260dabcb1e95bc4
Grant Wang <gw805@nyu.edu>
2018-10-27 01:09:38 -0700

    Saves because why not
    Yep!

---
+++ Action Adventure RPG/Assets/Scripts/Saving/GameData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+
+[System.Serializable]
+/// <summary>
+/// This is the save file that stores game info
+/// </summary>
+public class GameData {
+
+    public static GameData current;
+
+    public GameData() {
+
+    }
+}
+
+[System.Serializable]
+public class CharacterData {
+
+    public string name;
+
+    public int attack;
+    public int defense;
+    public int magicAttack;
+    public int magicDefense;
+    public int speed;
+    public int charisma;
+    
+    public CharacterData() {
+        this.name = "";
+    }
+}
+
+[System.Serializable]
+public class PlayerData : CharacterData{
+
+    public PlayerData() : base() {
+
+    }
+}
---
+++ Action Adventure RPG/Assets/Scripts/Saving/SaveManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+/// <summary>
+/// Manager that handles saving, deleting, and loading saves
+/// </summary>
+public static class SaveManager {
+
+    public static List<GameData> savedGames = new List<GameData>();
+
+    // adds a new saved game to the saves list
+    public static void SaveGame() {
+        savedGames.Add(GameData.current);
+        PersistSavedGames();
+    }
+
+    // overwrites a particular saved game in the saves list
+    public static void SaveGame(int index) {
+        if(index < 0 || index > savedGames.Count) { return; }
+        savedGames[index] = GameData.current;
+        PersistSavedGames();
+    }
+
+    // deletes a saved game from the list and persists the change to file
+    public static void DeleteSavedGame(int index) {
+        if (index < 0 || index > savedGames.Count) { return; }
+        savedGames.RemoveAt(index);
+        PersistSavedGames();
+    }
+
+    // writes the saved games list to file
+    private static void PersistSavedGames() {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/savedGames.gd");
+        bf.Serialize(file, savedGames);
+        file.Close();
+    }
+
+    // populates the savedGames list with data from  savedGames.gd
+    public static void LoadGamesList() {
+        if (File.Exists(Application.persistentDataPath + "/savedGames.gd")) {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            savedGames = (List<GameData>)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+}
